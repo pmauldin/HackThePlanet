3b085c098978a22d3f8a524644bfc02c4ea8c973
CamelBackNotation <JoshuaHurt@utexas.edu>
2015-08-15 18:55:28 -0500

    Added select_tool method which returns a string

--- blender/tools.py
+++ blender/tools.py
@@ -1,5 +1,15 @@
 import bpy
 
+# surface height = 120mm
+# surface width = 230mm
+# tool space width = 64
+TOOL_LIST = [
+		['VIEW_MOVE', 'VIEW_PAN', 'VIEW_ROTATE', 'OBJECT_ROTATED'],
+		['OBJECT_INDENT', 'VIEW_CURSOR', 'SENSITIVITY', 'RESET', 'PH']
+	]
+BUTTON_WIDTH = 120 / len(TOOL_LIST[0])
+BUTTON_HEIGHT = 230 / len(TOOL_LIST[1])
+
 WIDTH = 230
 HEIGHT = 120
 
@@ -19,4 +29,7 @@ def process_inputs(contacts, selected_tool):
 
 
 def select_tool(contact):
-	print("Selecting tool at %s, %s" % (contact.x_pos_mm, contact.y_pos_mm))
+	x_coord = contact.x_pos_mm
+	y_coord = contact.y_pos_mm
+	print("Selecting tool at %s, %s" % (x_coord, y_coord))
+	return TOOL_LIST[x_coord / BUTTON_WIDTH][y_coord / BUTTON_HEIGHT]
