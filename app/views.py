0239edef85a7d69ca59bfa0c233bb050fecfd58c
Tirey Morris <tireymorris@gmail.com>
2015-08-15 00:46:51 -0700

    Fixed views

--- app/views.py
+++ app/views.py
@@ -1,4 +1,7 @@
-from flask import render_template
+from flask import render_template, request, send_from_directory
 from app import app
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
