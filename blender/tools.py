3251783baae0262a630e392bf8e7309e316586c9
Peter Mauldin <pmauldin@snaptrends.com>
2015-08-15 17:28:00 -0700

    Fixed Josh's stupid shit

--- blender/tools.py
+++ blender/tools.py
@@ -3,17 +3,21 @@ import bpy
 # surface height = 120mm
 # surface width = 230mm
 # tool space width = 64
+import math
+
 TOOL_LIST = [
-		['VIEW_MOVE', 'VIEW_PAN', 'VIEW_ROTATE', 'OBJECT_ROTATED'],
-		['OBJECT_INDENT', 'VIEW_CURSOR', 'SENSITIVITY', 'RESET', 'PH']
-	]
-BUTTON_WIDTH = 120 / len(TOOL_LIST[0])
-BUTTON_HEIGHT = 230 / len(TOOL_LIST[1])
+		['VIEW_MOVE', 'VIEW_PAN', 'VIEW_ROTATE', 'VIEW_CURSOR'],
+		['OBJECT_INDENT', 'OBJECT_ROTATED', 'SENSITIVITY', 'RESET']
+]
 
 WIDTH = 230
 HEIGHT = 120
 
 TOOL_THRESHOLD = 65
+
+BUTTON_WIDTH = TOOL_THRESHOLD / 2
+BUTTON_HEIGHT = HEIGHT / len(TOOL_LIST[0])
+
 
 sensitivity = 80000
 selected_tool = "VIEW_CURSOR"
@@ -23,12 +27,14 @@ def process_inputs(contacts):
 
 	for contact in contacts:
 		if contact.x_pos_mm < TOOL_THRESHOLD:
-			selected_tool = select_tool(contact)
+			selected_tool = select_tool(contact.x_pos_mm, contact.y_pos_mm)
 			print("Selecting %s at %s, %s" % (selected_tool, contact.x_pos_mm, contact.y_pos_mm))
 			break
-		else:
-			print("Touching myself")
 
 
-def select_tool(contact):
-	return TOOL_LIST[int(contact.x_pos_mm / BUTTON_WIDTH)][int(contact.y_pos_mm / BUTTON_HEIGHT)]
+def select_tool(xpos, ypos):
+	x = math.floor(xpos / BUTTON_WIDTH)
+	print (x)
+	y = math.floor(ypos / BUTTON_HEIGHT)
+	print (y)
+	return TOOL_LIST[x][y]
